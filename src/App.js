0df9422415d9f4661ac789335ec4e7193472eef2
ClaudianyMartins <41713981+ClaudianyMartins@users.noreply.github.com>
2020-09-14 20:32:31 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>1 - npm install</h1>
+      <h2>2 - npm build</h2>
+      <h2>2 - npm start</h2>
+    </div>
+  );
+}
