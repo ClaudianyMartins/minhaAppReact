af83092f418c6f3290c16dee2e4c49839677c189
ClaudianyMartins <41713981+ClaudianyMartins@users.noreply.github.com>
2020-09-15 08:50:13 -0300

    alteracao na descriçao do passo 3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <h1>Passos para executar comandos</h1>
       <h2>1 - npm install</h2>
       <h2>2 - npm build</h2>
-      <h2>2 - npm start</h2>
+      <h2>3 - npm start</h2>
     </div>
   );
 }
