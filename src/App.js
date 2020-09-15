181bdd26afb402e8a9a95e2561881953e5efd84e
ClaudianyMartins <41713981+ClaudianyMartins@users.noreply.github.com>
2020-09-14 21:00:54 -0300

    adicionar titulo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>1 - npm install</h1>
+      <h1>Passos para executar comandos</h1>
+      <h2>1 - npm install</h2>
       <h2>2 - npm build</h2>
       <h2>2 - npm start</h2>
     </div>
